e8183588a9eb54d9c7a936c1c19de40509ee5115
jacekk024 <jacekkotra98@gmail.com>
2023-08-28 16:08:25 +0200

    Re-Update

--- ProjectsApi/ProjectsApi/Program.cs
+++ ProjectsApi/ProjectsApi/Program.cs
@@ -1,30 +1,25 @@
-using Microsoft.EntityFrameworkCore;
-using ProjectsApi.Data;
+var builder = WebApplication.CreateBuilder(args);
 
-internal class Program
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-        builder.Services.AddDbContext<ProjectContext>(options =>
-        options.UseSqlServer(
-        builder.Configuration.GetConnectionString("ProjectContext")));
-
-
-        builder.Services.AddControllers();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
