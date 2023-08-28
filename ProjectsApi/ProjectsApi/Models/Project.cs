a045681aeb30e2387344f897ab54644778235fa8
jacekk024 <jacekkotra98@gmail.com>
2023-08-28 17:27:26 +0200

    Re-Update correct migration files

---
+++ ProjectsApi/ProjectsApi/Models/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectsApi.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }   
+        public string Description { get; set; }
+        public string RepositoryLink {get; set; }
+        public string TechnologiesUsed { get; set; }
+        public DateTime DataAdded { get; set; }
+    }
+}
