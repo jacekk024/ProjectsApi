68be839a4bddde3b09472e5ee743742f094cd559
jacekk024 <jacekkotra98@gmail.com>
2023-09-01 00:42:39 +0200

    Update ProjectsController.cs

--- ProjectsApi/ProjectsApi/Controllers/ProjectsController.cs
+++ ProjectsApi/ProjectsApi/Controllers/ProjectsController.cs
@@ -1,15 +1,31 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
+using ProjectsApi.Data;
+using ProjectsApi.Models;
 namespace ProjectsApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ProjectsController : ControllerBase
     {
+        private readonly ProjectContext projectContext;
 
+        public ProjectsController(ProjectContext projectContext)
+        {
+            this.projectContext = projectContext;
+        }
 
+        // GET: api/Projects
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Project>>> GetProjects() 
+        {
+            if(projectContext == null)
+                return NotFound();
+
+            return await projectContext.Projects.ToListAsync(); 
+        
+        }
 
 
     }
