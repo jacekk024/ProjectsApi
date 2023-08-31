fd7d3d0d8192187759967e03f8858446d44878c3
jacekk024 <jacekkotra98@gmail.com>
2023-09-01 00:59:44 +0200

    Update Controller

--- ProjectsApi/ProjectsApi/Controllers/ProjectsController.cs
+++ ProjectsApi/ProjectsApi/Controllers/ProjectsController.cs
@@ -26,7 +26,43 @@ namespace ProjectsApi.Controllers
             return await projectContext.Projects.ToListAsync(); 
         
         }
+        // GET: api/Projects/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Project>> GetMovie(int id) 
+        {
+            if (projectContext == null)
+                return NotFound();
 
+            var project = await projectContext.Projects.FindAsync(id);
 
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return project;
+        }
+
+        //POST: api/Projects
+        public async Task<ActionResult<Project>> PostProject(Project project) 
+        {
+            if (project == null)
+            {
+                return BadRequest("Provided project is null.");
+            }
+
+            // Można dodać więcej walidacji dla 'project', np. sprawdzając czy pewne pola nie są puste.
+
+            try
+            {
+                projectContext.Projects.Add(project);
+                await projectContext.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(PostProject), new { id = project.Id }, project);
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
     }
 }
